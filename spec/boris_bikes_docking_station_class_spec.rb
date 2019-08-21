0f55c507e34be0ce6e2cb9ebcac8bbb2c28dddc1
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2019-08-21 16:00:12 +0100

    Step 10: add tests for working? and made tests pass

--- spec/boris_bikes_docking_station_class_spec.rb
+++ spec/boris_bikes_docking_station_class_spec.rb
@@ -5,4 +5,9 @@ describe "DockingStation" do
 describe '#release_bike'
   it {expect(DockingStation.new).to respond_to(:release_bike)}
 
+describe '#working?'
+  it {expect(DockingStation.new.release_bike).to respond_to(:working?)}
+
+describe '#working?'
+  it {expect(Bike.new.working?).to eq(true)}
 end
