747881bc2f53a6ab71b0fa5cc61080a2596fffc4
Elsie Ashworth <44971680+eashworth@users.noreply.github.com>
2019-08-21 18:06:35 +0100

    Step 12: Add&pass test: error message when bike released from empty station

--- lib/boris_bikes.rb
+++ lib/boris_bikes.rb
@@ -2,12 +2,19 @@ class DockingStation
   attr_reader :bike
 
   def release_bike
+    if bike == nil
+      raise ArgumentError.new("Docking station empty")
+    end
     Bike.new
   end
 
   def dock_bike(bike)
     @bike = bike
   end
+
+  # # def raise_error_when_empty
+  # #   raise_error when DockingStation.new.release_bike
+  # end
 
 end
 
--- spec/boris_bikes_docking_station_class_spec.rb
+++ spec/boris_bikes_docking_station_class_spec.rb
@@ -2,19 +2,21 @@ require 'boris_bikes'
 
 describe "DockingStation" do
 
-  describe '#release_bike'
+describe '#release_bike'
   it {expect(DockingStation.new).to respond_to(:release_bike)}
 
-  describe '#working?'
+describe '#working?'
   it {expect(DockingStation.new.release_bike).to respond_to(:working?)}
 
-  describe '#working?'
+describe '#working?'
   it {expect(Bike.new.working?).to eq(true)}
 
-  describe '#dock_bike'
+describe '#dock_bike'
   it {expect(DockingStation.new).to respond_to(:dock_bike)}
 
-  describe '#dock_bike'
+describe '#dock_bike'
   it {expect(bike = DockingStation.new.release_bike).to eq(DockingStation.new.dock_bike(bike))}
-  
+
+describe '#bike'
+  it { expect {DockingStation.new.release_bike}.to raise_error}
 end
